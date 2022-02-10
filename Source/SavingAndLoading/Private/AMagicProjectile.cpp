2ac3f7d89697ac06efc950d7685541f0f4fade86
ingmar-dejong <ingmar-dejong@hotmail.com>
2022-02-10 20:04:04 +0100

    Instigator toegevoegd

--- Source/SavingAndLoading/Private/AMagicProjectile.cpp
+++ Source/SavingAndLoading/Private/AMagicProjectile.cpp
@@ -32,7 +32,7 @@ AAMagicProjectile::AAMagicProjectile()
 
 void AAMagicProjectile::OnActorOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
-	if (OtherActor)
+	if (OtherActor && OtherActor != GetInstigator())
 	{
 		USAttributeComponent* AttributeComp = Cast<USAttributeComponent>(OtherActor->GetComponentByClass(USAttributeComponent::StaticClass()));
 		if (AttributeComp)
